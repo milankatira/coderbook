ad1f8f098b2d8814e6663a8701b209ec22eb2544
milan Katira <84834795+milankatira@users.noreply.github.com>
2022-03-24 23:08:28 +0530

    frontend coding module complete

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import Client from "../components/Client";
+import Editor from "./Editor";
 
 const EditorPage = () => {
+  const [client, setClient] = React.useState([
+    { socketId: 1, username: "gfgh" },
+    { socketId: 2, username: "gfgh" },
+    { socketId: 2, username: "gfgh" },
+    { socketId: 2, username: "gfgh" },
+    { socketId: 2, username: "gfgh" },
+    { socketId: 2, username: "gfgh" },
+    { socketId: 2, username: "gfgh" },
+    { socketId: 2, username: "gfgh" },
+  ]);
   return (
-    <div style={{color:"red"}}>EditorPage</div>
-  )
-}
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img className="logoImage" src="/code-sync.png" alt="logo-image" />
+          </div>
+          <h3>Connected</h3>
 
-export default EditorPage
+          <div className="clientsList">
+            {client.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
+        </div>
+        <button className="btn copyBtn">copy Room Id</button>
+
+        <button className="btn leaveBtn">Leave</button>
+      </div>
+
+      <div className="editorWrap">
+        <Editor/>
+      </div>
+    </div>
+  );
+};
+
+export default EditorPage;
